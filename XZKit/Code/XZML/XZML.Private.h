69634f68123737ecc1342c5b2ecbe3cf8ca1b4ce
Xezun <developer@xezun.com>
2020-08-19 00:08:54 +0800

    优化文案

--- XZKit/Code/XZML/XZML.Private.h
+++ XZKit/Code/XZML/XZML.Private.h
@@ -13,13 +13,13 @@ NS_ASSUME_NONNULL_BEGIN
 #define XZML_MAX_ATTRS 8
 
 typedef struct {
-    /// XZML 字符串
+    /// XZML 字符串。
     char const * const xzmlcs;
-    /// XZML 长度
+    /// XZML 长度。
     NSInteger const    length;
     /// 当前处理中的子字符串 range 。
     NSRange   srange;
-    /// 子字符串包含的属性个数（子字符串中 @ 的个数）
+    /// 子字符串包含的属性个数（子字符串中 @ 的个数）。
     NSInteger attspc;
     /// 每个属性的 range 。
     NSRange   attspr[XZML_MAX_ATTRS];
