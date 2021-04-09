39545967b84d9f4285574fde66e12922b4014f58
Xezun <developer@xezun.com>
2021-04-09 23:45:44 +0800

    XZKitOC

---
+++ XZKitOC/XZKitOC.h
@@ -0,0 +1,18 @@
+//
+//  XZKitOC.h
+//  XZKitOC
+//
+//  Created by Xezun on 2021/4/9.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XZKitOC.
+FOUNDATION_EXPORT double XZKitOCVersionNumber;
+
+//! Project version string for XZKitOC.
+FOUNDATION_EXPORT const unsigned char XZKitOCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XZKitOC/PublicHeader.h>
+
+
