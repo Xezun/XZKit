7ea2750ef03be5dd66a52984c7cb661672e55aec
Xezun <xezun@icloud.com>
2025-06-04 20:55:01 +0800

    优化：属性 IBOutlet 必须声明在 Class 上，在协议中声明，无法在 IB 中连线

--- XZKit/Code/ObjC/XZMocoa/GridView/GridView/View/XZMocoaGridView.h
+++ XZKit/Code/ObjC/XZMocoa/GridView/GridView/View/XZMocoaGridView.h
@@ -18,16 +18,17 @@ NS_ASSUME_NONNULL_BEGIN
 @optional
 /// 视图模型。
 @property (nonatomic, strong, nullable) __kindof XZMocoaGridViewModel *viewModel;
-/// 承载列表视图的可滚动的容器视图，一般为 UICollectionView 或 UITableView 视图。
-/// - 一般情况下，容器视图 contentView 的 delegate 和 dataSource 会被接管，请避免更改。
-/// - 由于标注了 IBOutlet 所以在 IB 中使用时，直接将视图关联到此属性即可。
-@property (nonatomic, strong) IBOutlet __kindof UIScrollView *contentView;
 /// 通过模块注册列表 Cell 视图。
 - (void)registerModule:(nullable XZMocoaModule *)module;
 @end
 
 /// XZMocoaGridView 是 UITableView、UICollectionView 的抽象封装，不可直接使用。
 @interface XZMocoaGridView : UIView <XZMocoaGridView>
+
+/// 承载列表视图的可滚动的容器视图，一般为 UICollectionView 或 UITableView 视图。
+/// - 一般情况下，容器视图 contentView 的 delegate 和 dataSource 会被接管，请避免更改。
+/// - 由于标注了 IBOutlet 所以在 IB 中使用时，直接将视图关联到此属性即可。
+@property (nonatomic, strong) IBOutlet __kindof UIScrollView *contentView;
 
 /// 提供子类用于监听 contentView 发生改变的方法。
 /// > 默认该方法什么也不做。
