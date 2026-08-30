85f8742b42cd3fdaf562780451a9b51e0fb7c2b3
Xezun <xezun@icloud.com>
2026-08-30 11:31:53 +0800

    重构

--- Sources/ObjC/XZMocoa/XZMocoaGroupView/Model/XZMocoaGroupModel.h
+++ Sources/ObjC/XZMocoa/XZMocoaGroupView/Model/XZMocoaGroupModel.h
@@ -15,6 +15,8 @@ NS_ASSUME_NONNULL_BEGIN
 /// 分组列表视图的数据模型，例如 UITableView、UICollectionView 的数据模型。
 ///
 /// 视图 XZMocoaGroupView 是 UITableView、UICollectionView 抽象，并非实际视图，不能直接使用。
+///
+/// 此协议提供了默认实现，所有 NSObject 子类对象，都可以作为列表数据模型。
 @protocol XZMocoaGroupModel <XZMocoaModel>
 @optional
 @property (nonatomic, readonly) NSInteger numberOfSections;
@@ -24,15 +26,14 @@ NS_ASSUME_NONNULL_BEGIN
 - (nullable id)modelForSupplementOfKind:(XZMocoaKind)kind atIndexPath:(NSIndexPath *)indexPath;
 @end
 
-/// 通过如下拓展，任何 NSObject 对象，都可以作为列表数据模型。
 @interface NSObject (XZMocoaGroupModel)
-/// 列表数据中子元素的数量。
+/// 对象作为列表数据时，列表中子元素的数量。
 @property (nonatomic, readonly) NSInteger xz_numberOfElements NS_SWIFT_NAME(numberOfElements);
-/// 位置 index 上的子元素的数据模型。
+/// 对象作为列表数据时，位置 index 上的子元素的数据模型。
 - (nullable id)xz_modelForElementAtIndex:(NSInteger)index NS_SWIFT_NAME(modelForElement(at:));
-/// 类型为 kind 的附加视图的数量。
+/// 对象作为列表 section 数据时，类型为 kind 的附加视图的数量。
 - (NSInteger)xz_numberOfSupplementsOfKind:(XZMocoaKind)kind NS_SWIFT_NAME(numberOfSupplements(of:));
-/// 位置 index 上，类型为 kind 的附加视图的数据模型。
+/// 对象作为列表 section 数据时，位置 index 上，类型为 kind 的附加视图的数据模型。
 - (nullable id)xz_modelForSupplementOfKind:(XZMocoaKind)kind atIndex:(NSInteger)index NS_SWIFT_NAME(modelForSupplement(of:at:));
 @end
 
